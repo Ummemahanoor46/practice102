619406a1e1716a9cad04f41d2e806b1c39f11e1f
Ummemahanoor46 <ummemamhanoor100@gmail.com>
2024-02-02 17:53:12 -0500

    xpath update

--- src/test/java/stepDef/loginSteps.java
+++ src/test/java/stepDef/loginSteps.java
@@ -12,26 +12,24 @@ public class loginSteps extends config {
 
     @Given("user in TalentTEK Homepage")
     public void userInTalentTEKHomepage() {
+        //SELENIUM CODES
     }
-
     @And("user enter valid email address")
     public void userEnterValidEmailAddress() {
         driver.findElement(By.name("email")).sendKeys(STUDENT_EMAIL);
     }
-
     @And("user enter valid password")
     public void userEnterValidPassword() {
         driver.findElement(By.name("password")).sendKeys(STUDENT_PASSWORD);
     }
-
     @When("user clicks on login button")
     public void userClicksOnLoginButton() {
-        driver.findElement(By.xpath("//*[@id='login_form']/input")).click();
+        driver.findElement(By.xpath("//input[@class='my-login']")).click();
     }
     @Then("the user should be able to login successfully")
     public void theUserShouldBeAbleToLoginSuccessfully() {
         String businessAct = "Student Information";
-        String developerCodeExp = driver.findElement(By.xpath("//*[@id='wrap']//h2")).getText();
+        String developerCodeExp = driver.findElement(By.xpath("//h2[@class='d-inline info']")).getText();
         Assert.assertEquals (businessAct, developerCodeExp);
     }
 }
